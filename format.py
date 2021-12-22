95e2117f2964cc6ec984c0a297a1cbaea28e8371
Milan <milanso@gmail.com>
2021-12-22 14:23:05 +0100

    s3

--- format.py
+++ format.py
@@ -1,3 +1,4 @@
+import boto3
 from fileinput import FileInput
 from os import write
 
@@ -6,5 +7,19 @@ w = open("proxies.txt", "w")
 with FileInput(files=['output.txt'], inplace=False) as f:
     for line in f:
         line = line.rstrip()
-        line = "\'http://"+line+"\'," 
+        line = "\'http://"+line+"\',"
         w.write(line+"\n")
+
+w.close()
+
+s3_client = boto3.client(
+    's3',
+    region_name='us-east-2',
+    aws_access_key_id='AKIA2W2BDOC4HMOMPP6P',
+    aws_secret_access_key='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',
+)
+
+with open("proxies.txt", "rb") as a:
+    s3_client.upload_fileobj(a, "moxyproxy", "proxies.txt")
+
+        
